dc9c2bfa94e0933b6dd8ade3f434f5121325e6e2
ivanjermakov <vania6600@gmail.com>
2019-11-24 18:18:53 +0300

    welcome screen

--- txtype.py
+++ txtype.py
@@ -57,11 +57,11 @@ def _print_input_win(win, status_win, text, input_text, key):
     win.clear()
 
     if text.has_next():
+        _paint_bar(win, status_win, curses.color_pair(0))
+        win.addstr(0, 0, input_text[-w + 1:])
+
         if input_text:
-            if text.words[text.current_word_index].word_str.startswith(input_text):
-                _paint_bar(win, status_win, curses.color_pair(0))
-                win.addstr(0, 0, input_text[-w + 1:])
-            else:
+            if not text.words[text.current_word_index].word_str.startswith(input_text):
                 _paint_bar(win, status_win, curses.color_pair(3))
                 win.addstr(0, 0, input_text[-w + 1:])
         win.refresh()
@@ -93,16 +93,42 @@ def _refresh_all(screen, text_win, input_win, status_win):
     status_win.refresh()
 
 
-def main(screen):
-    curses.curs_set(0)
-    curses.use_default_colors()
-    curses.cbreak()
+def _show_welcome(screen):
+    h, w = screen.getmaxyx()
 
-    curses.init_pair(1, curses.COLOR_RED, -1)  # error
-    curses.init_pair(2, curses.COLOR_GREEN, -1)  # correct
-    curses.init_pair(3, -1, curses.COLOR_RED)  # input error
-    curses.init_pair(4, -1, curses.COLOR_GREEN)  # text complete
+    screen.clear()
 
+    lines = [
+        '          Welcome to TXTYPE!          ',
+        '                                      ',
+        '     Command line typing software     ',
+        'https://github.com/ivanjermakov/txtype',
+        '                                      ',
+        'Help:                                 ',
+        '     ctrl+z       force exit          ',
+        '     q            exit                ',
+        '     n            next text           ',
+        '     w            welcome page        '
+    ]
+
+    if h > 9 and w > 38:
+        y = h // 2 - len(lines) // 2
+        x = w // 2 - len(lines[0]) // 2
+
+        for i, l in enumerate(lines):
+            screen.insstr(y + i, x, l)
+
+        # accents
+        screen.addstr(y, x + 21, 'TXTYPE', curses.color_pair(2))
+        screen.addstr(y + 6, x + 5, 'ctrl+z', curses.color_pair(2))
+        screen.addstr(y + 7, x + 5, 'q', curses.color_pair(2))
+        screen.addstr(y + 8, x + 5, 'n', curses.color_pair(2))
+        screen.addstr(y + 9, x + 5, 'w', curses.color_pair(2))
+
+    screen.refresh()
+
+
+def _show_text(screen):
     h, w = screen.getmaxyx()
     screen.refresh()
 
@@ -121,21 +147,21 @@ def main(screen):
     while True:
         key = screen.get_wch()
 
-        if curses.is_term_resized(h, w) is True:
+        if key == curses.KEY_RESIZE:
             h, w = screen.getmaxyx()
             screen.clear()
             text_win = _init_text_win(h, w)
             input_win = _init_input_win(h, w)
             status_win = _init_status_win(h, w)
-            _refresh_all(screen, text_win, input_win, status_win)
             _print_text_win(text_win, text)
             _print_input_win(input_win, status_win, text, input_text, key)
-            curses.resize_term(h, w)
+            _refresh_all(screen, text_win, input_win, status_win)
 
         if text.current_word_index == 0 and len(input_text) == 0:
             text.start_typing()
 
-        # TODO: fix ctrl-v OP strategy. Disable copying and/or pasting. Or mouse actions overall
+        if key == curses.KEY_MOUSE:
+            pass
         if key == '\n' or key == ' ':
             if text.has_next():
                 text.next(input_text)
@@ -148,12 +174,39 @@ def main(screen):
         _print_text_win(text_win, text)
         _print_status_win(status_win, text)
 
-        key = _print_input_win(input_win, status_win, text, input_text, key)
-        if key:
-            if key == 'n':
-                main(screen)
-            if key == 'q':
-                return
+        return _print_input_win(input_win, status_win, text, input_text, key)
+
+
+def main(screen):
+    curses.mousemask(1)
+    curses.curs_set(0)
+    curses.use_default_colors()
+    curses.cbreak()
+
+    curses.init_pair(1, curses.COLOR_RED, -1)  # error
+    curses.init_pair(2, curses.COLOR_GREEN, -1)  # correct
+    curses.init_pair(3, -1, curses.COLOR_RED)  # input error
+    curses.init_pair(4, -1, curses.COLOR_GREEN)  # text complete
+
+    _show_welcome(screen)
+
+    while True:
+        key = screen.get_wch()
+        if key == curses.KEY_RESIZE:
+            _show_welcome(screen)
+        elif key == 'n':
+            break
+        elif key == 'q':
+            return
+
+    key = _show_text(screen)
+    if key:
+        if key == 'n':
+            _show_text(screen)
+        if key == 'w':
+            main(screen)
+        if key == 'q':
+            return
 
 
 curses.wrapper(main)
